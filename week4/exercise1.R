742fb4a45439e4862ac41a88dacb5142c4fc37e9
Guido Schlögel <guido.schlogel@gmx.net>
2021-03-14 15:00:04 +0100

    alternative solutions for 1 (d)

--- week4/exercise1.R
+++ week4/exercise1.R
@@ -34,3 +34,14 @@ y <- data.frame(smoker = 'yes', age = 50, bmi = bmi50)
 
 predict(model2, x)
 predict(model2, y)
+
+# alternative solution, create model to predict bmi at certain age
+
+model.bmi <- lm(bmi ~ age, patients)
+predict(model2, data.frame(age = 20, smoker = 'no', bmi = predict(model.bmi, data.frame(age = 20))))
+predict(model2, data.frame(age = 50, smoker = 'yes', bmi = predict(model.bmi, data.frame(age = 50))))
+
+# solution 3 create model without bmi
+model3 <- lm(charges ~ age + smoker, patients)
+predict(model3, data.frame(age = 20, smoker = 'no'))
+predict(model3, data.frame(age = 50, smoker = 'yes'))
